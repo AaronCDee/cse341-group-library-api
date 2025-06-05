176aaa9d39e40e2f297711f19bcb17af89a7148c
AaronCDee <101570796+AaronCDee@users.noreply.github.com>
2025-06-05 06:59:48 +0200

    add validations for all routes (#7)

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,50 +1,100 @@
 const validator = require('../helpers/validate');
 
-// const saveContact = (req, res, next) => {
-//   const validationRule = {
-//     firstName: 'required|string',
-//     lastName: 'required|string',
-//     email: 'required|email',
-//     favoriteColor: 'required|string',
-//     birthday: 'string'
-//   };
-//   validator(req.body, validationRule, {}, (err, status) => {
-//     if (!status) {
-//       res.status(412).send({
-//         success: false,
-//         message: 'Validation failed',
-//         data: err
-//       });
-//     } else {
-//       next();
-//     }
-//   });
-// };
+const saveMember = (req, res, next) => {
+  const validationRule = {
+    firstName: 'required|string',
+    lastName: 'required|string',
+    address: 'required|string',
+    email: 'required|string',
+    phone: 'required|string',
+    birthday: 'required|string',
+    borrowed: 'string',
+    feeBalance: 'required|numeric',
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
 
-// const saveBook = (req, res, next) => {
-//   const validationRule = {
-//     ISBN: 'required|string',
-//     Title: 'required|string',
-//     Author: 'required|string',
-//     publicationYear: 'required|integer',
-//     Genre: 'string',
-//     Pages: 'integer',
-//     userRating: 'numeric'
-//   };
-//   validator(req.body, validationRule, {}, (err, status) => {
-//     if (!status) {
-//       res.status(412).send({
-//         success: false,
-//         message: 'Validation failed',
-//         data: err
-//       });
-//     } else {
-//       next();
-//     }
-//   })
-// }
+const saveUser = (req, res, next) => {
+  const validationRule = {
+    firstName: 'required|string',
+    lastName: 'required|string',
+    email: 'required|string',
+    phone: 'required|string',
+    position: 'required|string',
+    startDate: 'required|string',
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+const saveBook = (req, res, next) => {
+  const validationRule = {
+    ISBN: 'required|string',
+    Title: 'required|string',
+    Author: 'required|string',
+    publicationYear: 'required|integer',
+    Genre: 'string',
+    Pages: 'integer',
+    userRating: 'numeric'
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  })
+}
+
+const saveMovie = (req, res, next) => {
+  const validationRule = {
+    ISBN: 'required|string',
+    title: 'required|string',
+    director: 'required|string',
+    year: 'required|integer',
+    genre: 'string',
+    distributor: 'string',
+    userRating: 'numeric'
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  })
+}
 
 module.exports = {
-  saveContact,
-  saveBook
+  saveMember,
+  saveUser,
+  saveBook,
+  saveMovie,
 };
