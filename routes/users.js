f9ed97f54d78c96c44d991286e1ec494c71edb7f
hwcall1102 <haydenc0293@gmail.com>
2025-06-04 22:18:17 -0600

    routes controllers

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/users');
+// const validation = require('../middleware/validate');
+const { isAuthenticated }= require('../middleware/authenticate')
+
+router.get('/', usersController.getAll);
+
+router.get('/:id', usersController.getSingle);
+
+router.post('/', isAuthenticated, usersController.createUser)
+router.put('/:id', isAuthenticated, usersController.updateUser);
+router.delete('/:id', isAuthenticated, usersController.deleteUser);
+
+module.exports = router;
